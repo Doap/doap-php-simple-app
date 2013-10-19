6a1b914f7ba2ecd10ebe50ba9ee71d71591c38f3
David Menache <dave@doap.com>
2013-10-19 00:29:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
         <div class="container">
             <div class="hero-unit">
                 <h2>This is probably not what you were expecting.</h2>
-                <p>To enjoy the full features from the <code>version2</code> branch of the <code>opsworks-demo-php-simple-app</code> sample, you need to go to your OpsWorks stack and set your app's <strong>Document root</strong> to <strong>web</strong> and redeploy the app. Hurry back to see the real app at work!</p>
+                <p>To enjoy the full features from the <code>version2</code> branch of the <code>doap-wordpress-app</code> sample, you need to go to your OpsWorks stack and set your app's <strong>Document root</strong> to <strong>web</strong> and redeploy the app. Hurry back to see the real app at work!</p>
                 
-                <h3>Need more help?</h3>
+                <h3>Deploy Walkthru</h3>
                 <ul>
                     <li>For info on how to deploy an app, see <a href="http://docs.aws.amazon.com/opsworks/latest/userguide/workingapps-deploying.html">Deploying Apps</a> in the docs.</li>
                     <li>For more info on this sample and how it works, see <a href="http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.phpapp.2.html">Step 2. Update the server application to a new version that uses a database</a>.</li>
@@ -34,7 +34,7 @@
         <h2>DevOps and Platforms</h2>
         <P>Welcome to another Doap website:</p>
         <ul>
-            <li>Repo is here: <a href="https://github.com/Doap/doap-php-simple-app.git">GitHub Doap simple app repo</a></a></li>
+            <li>Repo is here: <a href="https://github.com/Doap/doap-wordpress-app.git">GitHub doap-wordpress-app repo</a></a></li>
             <li>Click <a href="info.php">here</a> to try some php.</li>
             <li><a href=http://50.112.224.71/>http://50.112.224.71/</a></li>
             <li><a href=mysql://54.244.200.191/>mysql://54.244.200.191/</a></li>
