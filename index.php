5b2e5d3d706d655268e341ab8633519bc0355e6a
David Menache <dave@doap.com>
2013-10-18 23:49:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Simple PHP App</title>
+        <title>Simple PHP App version 2</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="web/assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #333;}</style>
@@ -24,7 +24,29 @@
                     <li>For more info on this sample and how it works, see <a href="http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.phpapp.2.html">Step 2. Update the server application to a new version that uses a database</a>.</li>
                 </ul>
             </div>
+
+
+    <div style="max-width:960px;">
+        <div style="position:relative;float:right;margin-right: 10px;width:168px;">
+            <img style="position:relative;float:right;margin-right: 10px;" alt="doapy128sq" src="http://doap.com/wp-content/uploads/2013/05/doapy128sq.png" width="168" height="168">
         </div>
+   
+    <h2>DevOps and Platforms</h2>
+    <P>Welcome to another Doap website:</p>
+        <ul>
+            <li>Repo is here: <a href="https://github.com/Doap/doap-php-simple-app.git">GitHub Doap simple app repo</a></a></li>
+            <li>Click <a href="info.php">here</a> to try some php.</li>
+            <li><a href=http://50.112.224.71/>http://50.112.224.71/</a></li>
+            <li><a href=mysql://54.244.200.191/>mysql://54.244.200.191/</a></li>
+        </ul>
+    </div>
+
+
+
+        </div>
+        
+
+
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script src="web/assets/js/bootstrap.min.js"></script>
